c7828b624daa1a5c56751f44871ef684bd19f041
Lucas Nogueira <lucas_snogueira@hotmail.com>
2019-08-31 22:23:18 -0300

    feat: adiciona tela de login, tema e módulo do angular material

--- src/app/presentation/view/pages/pages.module.ts
+++ src/app/presentation/view/pages/pages.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PagesRoutingModule } from './pages-routing.module';
+import { LoginComponent } from './login/login.component';
+import { AppMaterialModule } from 'src/app/app-material.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent],
   imports: [
     CommonModule,
-    PagesRoutingModule
+    PagesRoutingModule,
+    AppMaterialModule
   ]
 })
 export class PagesModule { }
